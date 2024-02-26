a0fc10d9c26e068960ba24b39b58fd01d6fce77d
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-02-26 06:20:14 +0100

    feat: temp return from path on stop

--- line_follower.py
+++ line_follower.py
@@ -1,3 +1,4 @@
+from time import sleep
 from controllers import motor_controller, gyro_controller, ir_controller
 from intersection_actions import IntersectionAction
 
@@ -46,8 +47,8 @@ class LineFollower:
             # print("current angle: %f" % gyro.get_ang_z())
             # print("target angle: %f" % target_angle)
         self.motor_controller.stop()
-        print("target_angle reached")
-        print("current angle z: %f" % self.gyro_controller.get_ang_z())
+        # print("target_angle reached")
+        # print("current angle z: %f" % self.gyro_controller.get_ang_z())
 
     def rotate_to_detect_line(self, direction):
         while (not self.is_line_detected()):
@@ -61,8 +62,8 @@ class LineFollower:
         # get the current z angle and map it to range(0, 360)
         angle_z = self.gyro_controller.get_ang_z()
         mapped_angle_z = map(angle_z, -180, 180, 0, 360)
-        print("current angle z: %f" % angle_z)
-        print("mapped current angle z: %f" % mapped_angle_z)
+        # print("current angle z: %f" % angle_z)
+        # print("mapped current angle z: %f" % mapped_angle_z)
 
         # calculate the target angle and map it to range(-180, +180)
         mapped_target_angle = 0
@@ -72,21 +73,21 @@ class LineFollower:
             mapped_target_angle = (mapped_angle_z - turning_angle) % 360
 
         target_angle = map(mapped_target_angle, 0, 360, -180, 180)
-        print("mapped target_angle: %f" % mapped_target_angle)
-        print("target_angle: %f" % target_angle)
+        # print("mapped target_angle: %f" % mapped_target_angle)
+        # print("target_angle: %f" % target_angle)
 
         # rotate until target angle is reached
-        print("rotating until angle matched")
+        # print("rotating until angle matched")
         self.rotate_to_match_target_angle(target_angle, direction)
 
         # at this point it is possible that the car is not on the line
         # so we make it rotate in the same direction as before until
         # the car is back on the line
         if (not self.is_line_detected()):
-            print("rotating until car on line")
+            # print("rotating until car on line")
             self.rotate_to_detect_line(direction)
-            print("car on line")
-            print("current angle z: %f" % self.gyro_controller.get_ang_z())
+            # print("car on line")
+            # print("current angle z: %f" % self.gyro_controller.get_ang_z())
 
     def rotate_to_opposite_direction(self, direction):
         self.rotate(direction, OPPOSITE_DIRECTION_TURNING_ANGLE)
@@ -94,30 +95,43 @@ class LineFollower:
     def switch_to_new_lane(self, direction):
         self.rotate(direction, NEW_LINE_TURNING_ANGLE)
 
-    def do_follow_line(self, intersection_list: list[IntersectionAction]):
-        self.intersection_list = intersection_list
+    def new_path(self, intersection_list: list[IntersectionAction]):
+        self.do_follow_line(intersection_list=intersection_list)
 
-        while True:
+    def continue_path(self):
+        self.do_follow_line(intersection_list=None)
+
+    def do_follow_line(self, intersection_list: list[IntersectionAction] = None):
+        if intersection_list is not None:
+            self.reset_actions()
+            self.intersection_list = intersection_list
+
+        print(self.intersection_list)
+        print(self.intersection_idx)
+        print(len(self.intersection_list) - 1)
+
+        # while True:
+        while self.intersection_idx < len(self.intersection_list):
             ir_values = self.ir_controller.get_ir_values()
 
             if (ir_values == 0): # 000
                 self.motor_controller.stop()
 
                 if (self.last_action == "slr"):
-                    print("overshoot")
-                    print("rotating until car on line")
+                    # print("overshoot")
+                    # print("rotating until car on line")
                     self.rotate_to_detect_line("l")
-                    print("car on line")
+                    # print("car on line")
                 elif (self.last_action == "sll"):
-                    print("overshoot")
-                    print("rotating until car on line")
+                    # print("overshoot")
+                    # print("rotating until car on line")
                     self.rotate_to_detect_line("r")
-                    print("car on line")
+                    # print("car on line")
                 elif (self.last_action == "ta"):
-                    print("overshoot")
-                    print("rotating until car on line")
+                    # print("overshoot")
+                    # print("rotating until car on line")
                     self.rotate_to_detect_line("r")
-                    print("car on line")
+                    # print("car on line")
 
                 if (self.still_on_intersection):
                     self.still_on_intersection = False
@@ -142,6 +156,8 @@ class LineFollower:
                     continue
 
                 action = self.intersection_list[self.intersection_idx]
+                print(self.intersection_idx)
+                print(action)
 
                 if (action == IntersectionAction.RIGHT):
                     print("switching to new lane - right")
@@ -170,9 +186,13 @@ class LineFollower:
                     print("")
                     self.still_on_intersection = False
                     self.last_action = "sll"
-                    self.intersection_idx += 1
+                    self.intersection_idx += 2
                 elif (action == IntersectionAction.STOP):
                     print("stop reached")
                     self.motor_controller.stop()
-                    self.reset_actions()
-                    break
+                    self.intersection_idx += 1
+                    return
+                    # sleep(5)
+        
+        self.motor_controller.stop()
+        self.reset_actions()
